a8b5db899523b4fa371cd5de678ed2c30a6c362a
Norskeaksel <ahsorbye@gmail.com>
2024-12-06 19:19:51 +0100

    Add topological sorting

--- src/main/kotlin/DFS.kt
+++ src/main/kotlin/DFS.kt
@@ -1,6 +1,7 @@
 class DFS(val graph: AdjacencyList) {
     val size = graph.size
     var visited = BooleanArray(size)
+    var prossessed = mutableListOf<Int>()
     var depth = 0
     private var currentVisited = mutableListOf<Int>()
 
@@ -37,8 +38,16 @@ class DFS(val graph: AdjacencyList) {
                     this.callRecursive(v)
                 }
             }
+            prossessed.add(id)
         }.invoke(start)
         depth = currentVisited.size
+    }
+
+    fun topologicalSort(): List<Int> {
+        for(i in 0 until size){
+            dfsRecursive(i)
+        }
+        return prossessed.reversed() //Reversed depending on the order
     }
     fun getCurrentVisited() = // Deep Copy
         currentVisited.map { it }.toList()
