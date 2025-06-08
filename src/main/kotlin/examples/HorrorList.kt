fc1e56d0ec9634b24f348fd1f58c1d08d71ee3bc
Norskeaksel <ahsorbye@gmail.com>
2025-06-08 11:34:40 +0200

    Fix BFS bug and solve HorrorList

---
+++ src/main/kotlin/examples/HorrorList.kt
@@ -0,0 +1,22 @@
+package examples
+
+import graphClasses.*
+// https://open.kattis.com/problems/horror?editresubmit=17527573
+fun main() {
+    val ans = horrorList(); _writer.flush()
+    println(ans)
+}
+
+fun horrorList(): Int {
+    val (n, h, l) = readInts(3)
+    val intGraph = IntGraph(n)
+    val startIds = readInts(h).sorted()
+    repeat(l) {
+        val (u, v) = readInts(2)
+        intGraph.connect(u, v)
+    }
+    val bfs = BFS(intGraph)
+    bfs.bfsRecursive(startIds)
+    val maxDist = bfs.distances.let { d -> d.indices.maxBy { d[it] } }
+    return maxDist
+}
