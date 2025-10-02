9dd9c5d18c5d201c45de70f21b5196284f537c57
Norskeaksel <ahsorbye@gmail.com>
2025-10-02 09:21:13 +0200

    Add Watersheds example

---
+++ src/main/kotlin/examples/Watersheds.kt
@@ -0,0 +1,51 @@
+package examples
+
+import graphClasses.Grid
+import graphClasses.Tile
+import readInt
+import readInts
+
+// https://open.kattis.com/problems/watersheds
+fun main() {
+    val ans = watersheds()
+    println(ans)
+}
+
+fun watersheds(): String {
+    val n = readInt()
+    var ans = ""
+    repeat(n) { caseNr ->
+        val (h, w) = readInts(2)
+        val grid = Grid(height = h, width = w)
+        repeat(h) { y ->
+            repeat(w) { x ->
+                val data = readInt()
+                grid.addNode(Tile(x, y, data))
+            }
+        }
+        grid.connectGrid(true) { t ->
+            val drainage = grid.getStraightNeighbours(t).filter {
+                (it.data as Int) < (t.data as Int)
+            }
+            if (drainage.isEmpty()) listOf() else
+                listOf(drainage.minBy { it.data as Int })
+        }
+        val scc = grid.stronglyConnectedComponents()
+        val sccSorted = scc.sortedBy { component -> component.minOf { it.x + w * it.y } }
+        sccSorted.forEachIndexed { i, component ->
+            component.forEach { t ->
+                grid.addNode(Tile(t.x, t.y, 'a' + i))
+            }
+        }
+        val sb = StringBuilder()
+        sb.append("Case #${caseNr + 1}:\n")
+        grid.nodes().forEachIndexed { id, t ->
+            if (id > 0 && id % w == 0)
+                sb.append("\n")
+            sb.append(t.data.toString()).append(" ")
+        }
+        sb.append("\n")
+        ans += sb.toString()
+    }
+    return ans.trimEnd()
+}
---
+++ src/test/kotlin/TestWatersheds.kt
@@ -0,0 +1,54 @@
+import examples.watersheds
+import graphClasses.*
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterAll
+import java.io.File
+import org.junit.jupiter.api.Test
+import kotlin.system.measureTimeMillis
+
+class WatershedsTest {
+    companion object {
+        @JvmStatic
+        @AfterAll
+        fun resetInput() {
+            _reader = INPUT.bufferedReader()
+        }
+    }
+
+    @Test
+    fun watershedsa() {
+        val expectedOutput = """Case #1:
+a b b 
+a a b 
+a a a 
+Case #2:
+a a a a a a a a a b 
+Case #3:
+a a a 
+b b b 
+Case #4:
+a a a a a 
+a a b b a 
+a b b b a 
+a b b b a 
+a a a a a 
+Case #5:
+a b c d e f g h i j k l m 
+n o p q r s t u v w x y z 
+Case #6:
+a a a 
+b a a 
+b a c"""
+        _reader = File("src/test/SampleInput/Watersheds/input1").inputStream().bufferedReader()
+        assertThat(watersheds()).isEqualTo(expectedOutput)
+    }
+
+    @Test
+    fun watershedSpeedTest() {
+        _reader = File("src/test/SampleInput/Watersheds/input2").inputStream().bufferedReader()
+        val time = measureTimeMillis {
+            watersheds()
+        }
+        debug("Watersheds took $time ms")
+    }
+}
