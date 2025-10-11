9097d353ccff5aa736727da3cec010425de303f0
Norskeaksel <ahsorbye@gmail.com>
2025-10-11 11:33:36 +0200

    Add builddeps example

---
+++ src/main/kotlin/examples/Builddeps.kt
@@ -0,0 +1,37 @@
+package examples
+
+import graphClasses.Graph
+import readInt
+import readString
+
+// https://open.kattis.com/problems/builddeps
+fun main() {
+    val ans = builddeps()
+    println(ans)
+}
+
+fun builddeps(): String {
+    val n = readInt()
+    val g = Graph()
+    repeat(n) {
+        val words = readString().split(" ")
+        val file = words[0].removeSuffix(":")
+        val dependencies = if (words.size > 1) words.subList(1, words.size) else emptyList()
+        dependencies.forEach { dependency ->
+            g.addUnweightedEdge(dependency, file)
+        }
+    }
+    val changedFile = readString()
+    g.addNode(changedFile)
+    g.dfs(changedFile)
+    val reachableNodes = g.currentVisitedNodes().toSet()
+    val order = g.topologicalSort().reversed()
+    val ans = StringBuilder()
+    order.forEach {file ->
+        if(file in reachableNodes)
+            ans.appendLine(file)
+    }
+    // debug(affectedFiles)
+    // g.visualize()
+    return ans.toString()
+}
---
+++ src/test/kotlin/TestBuilddeps.kt
@@ -0,0 +1,27 @@
+import examples.builddeps
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterAll
+import java.io.File
+import org.junit.jupiter.api.Test
+
+class BuilddepsTest {
+    companion object {
+        @JvmStatic
+        @AfterAll
+        fun resetInput() {
+            _reader = INPUT.bufferedReader()
+        }
+    }   
+
+    @Test
+    fun builddepsa() {
+        val expectedOutput = """gmp
+map
+set
+solution
+"""
+        _reader = File("src/test/SampleInput/Builddeps/input1").inputStream().bufferedReader()
+        assertThat(builddeps()).isEqualTo(expectedOutput)
+    }
+
+}
