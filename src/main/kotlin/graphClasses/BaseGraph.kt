24cfd4af420d8ba0e124ddb41eddc3909ac25aae
Norskeaksel <ahsorbye@gmail.com>
2025-10-19 14:53:47 +0200

    Wip docstrings

--- src/main/kotlin/graphClasses/BaseGraph.kt
+++ src/main/kotlin/graphClasses/BaseGraph.kt
@@ -1,7 +1,6 @@
 package graphClasses
 
 import AdjacencyList
-import Edge
 import UnweightedAdjacencyList
 import graphAlgorithms.*
 import toUnweightedAdjacencyList
@@ -12,7 +11,6 @@ abstract class BaseGraph<T : Any>(size: Int, val isWeighted: Boolean = true) {
     // PROPERTIES AND INITIALIZATION
     protected val adjacencyList: AdjacencyList = MutableList(size) { mutableListOf() }
     protected var unweightedAdjacencyList: UnweightedAdjacencyList = MutableList(size) { mutableListOf() }
-    private val connections = mutableListOf<Pair<T, T>>()
 
     protected var nodes: MutableList<T?> = MutableList(size) { null }
     private var searchResults: GraphSearchResults? = null
@@ -28,56 +26,63 @@ abstract class BaseGraph<T : Any>(size: Int, val isWeighted: Boolean = true) {
     abstract fun nodes(): List<T>
 
     // CORE GRAPH OPERATIONS
-
+    /** Adds an edge between two nodes in the graph, and creates the nodes if they don't exist.
+     *
+     * If the graph is unweighted, the edge is added without a weight. If a weight is provided
+     * for an unweighted graph, an error is thrown. If the graph is weighted, a weight must be provided;
+     * otherwise, an error is thrown.
+     *
+     * @param node1 The starting node of the edge.
+     * @param node2 The ending node of the edge.
+     * @param weight The weight of the edge (required for weighted graphs). Defaults to `null`.
+     * @throws IllegalStateException If a weight is provided for an unweighted graph or if no weight
+     * is provided for a weighted graph. */
     fun addEdge(node1: T, node2: T, weight: Number? = null) {
-        if (!isWeighted) addUnweightedEdge(node1, node2)
-        else if (weight == null) error(
+        if (!isWeighted) {
+            if (weight != null)
+                error(
+                    "A weight cannot be given to a unweighted graph. Don't provide a weight or make the graph " +
+                            "unweighted by setting the parameter isWeighted=true when creating it."
+                )
+            addUnweightedEdge(node1, node2)
+        } else if (weight == null) error(
             "A weight must be provided when adding edges in a weighted graph. " +
                     "To make the graph unweighted, set the parameter isWeighted=false when creating it."
         )
         else addWeightedEdge(node1, node2, weight.toDouble())
     }
 
+    /** @return The total number of nodes in the graph. */
     fun size() = nodes().size
+
+    /** Connects two nodes in the graph, by calling addEdge(node1,node2, weight) and addEdge(node2, node1, weight)
+     *
+     * @param node1 The first node to connect.
+     * @param node2 The second node to connect.
+     * @param weight The weight of the connection (required for weighted graphs). Defaults to `null`.
+     * @throws IllegalStateException If no weight is provided for a weighted graph. */
     fun connect(node1: T, node2: T, weight: Number? = null) {
-        if (!isWeighted) connectUnweighted(node1, node2)
-        else if (weight == null) error(
-            "A weight must be provided when connecting nodes in a weighted graph. " +
-                    "To make the graph unweighted, set the parameter isWeighted=false when creating it."
-        )
-        else connectWeighted(node1, node2, weight.toDouble())
+        addEdge(node1, node2, weight)
+        addEdge(node2, node1, weight)
     }
 
-    private fun connectWeighted(node1: T, node2: T, weight: Double) {
-        addWeightedEdge(node1, node2, weight)
-        addWeightedEdge(node2, node1, weight)
-        connections.add(node1 to node2)
-    }
-
-    private fun connectUnweighted(node1: T, node2: T) {
-        addUnweightedEdge(node1, node2)
-        addUnweightedEdge(node2, node1)
-        connections.add(node1 to node2)
-    }
-
-    fun removeEdge(node1: T, node2: T, weight: Double? = null) {
+    /** Removes the edge(s) between two nodes in the graph.
+     *
+     * @param node1 The first node of the edge to remove.
+     * @param node2 The second node of the edge to remove.
+     * @throws IllegalStateException If either node is not found in the graph. */
+    fun removeEdge(node1: T, node2: T) {
         val u = node2Id(node1) ?: error("Node $node1 not found in graph")
         val v = node2Id(node2) ?: error("Node $node2 not found in graph")
-        removeEdge(u, v, weight)
+        removeEdge(u, v)
     }
 
-    private fun removeEdge(id1: Int, id2: Int, weight: Double? = null) {
-        if (weight == null)
-            adjacencyList[id1].removeAll { it.second == id2 }
-        else
-            adjacencyList[id1].remove(Edge(weight, id2))
-        if (nrOfConnections(unweightedAdjacencyList) > nrOfConnections(adjacencyList)) {
-            unweightedAdjacencyList[id1].remove(id2)
-        }
+    private fun removeEdge(id1: Int, id2: Int) {
+        if (isWeighted) adjacencyList[id1].removeAll { it.second == id2 }
+        else unweightedAdjacencyList[id1].remove(id2)
     }
 
     // GRAPH INFORMATION
-    fun connections() = connections.toList()
     fun depth() = searchResults?.depth ?: error("Can't retrieve depth because no search has been run yet")
     fun visitedNodes() = searchResults?.run { visited.indices.mapNotNull { if (visited[it]) id2Node(it) else null } }
         ?: error("Can't retrieve visitedNodes because no search has been run yet")
@@ -115,13 +120,13 @@ abstract class BaseGraph<T : Any>(size: Int, val isWeighted: Boolean = true) {
             ?: error("Haven't computed furthest node because no search algorithm (dfs, bfs, dijkstra) has been run yet.")
 
 
-
-    fun edges(t:T): List<Pair<Double, T>> = if(isWeighted) weightedEdges(t)
+    fun edges(t: T): List<Pair<Double, T>> = if (isWeighted) weightedEdges(t)
     else neighbours(t).map { 1.0 to it }
 
     private fun weightedEdges(t: T): List<Pair<Double, T>> =
         node2Id(t)?.let { adjacencyList[it] }?.map { Pair(it.first, id2Node(it.second)!!) }
             ?: error("Node $t not found in graph")
+
     fun neighbours(t: T): List<T> =
         if (isWeighted) weightedEdges(t).map { it.second }
         else node2Id(t)?.let { unweightedAdjacencyList[it] }
