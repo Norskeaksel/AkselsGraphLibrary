4004415bcc8e54e4d29718b7aab696bf5b02daf7
Norskeaksel <ahsorbye@gmail.com>
2025-10-20 17:24:40 +0200

    Add docstrings

--- src/main/kotlin/graphGraphics/VisualizeStronglyConnectedComponens.kt
+++ src/main/kotlin/graphGraphics/VisualizeStronglyConnectedComponens.kt
@@ -4,6 +4,10 @@ import Components
 import IntComponents
 import graphClasses.Graph
 
+/** Visualizes the strongly connected components (SCCs) of a graph.
+ *
+ * This function creates a new graph where each component is represented as a cycle,
+ * connecting its nodes in a circular manner. The graph is then printed and visualized. */
 fun Components.visualizeComponents() {
     val sccGraph = Graph()
     forEach { component ->
@@ -17,4 +21,7 @@ fun Components.visualizeComponents() {
     sccGraph.visualize()
 }
 
+/** Visualizes the strongly connected components (SCCs) of a graph with integer nodes.
+ *
+ * This function is a specialized version of `visualizeComponents` for integer-based components. */
 fun IntComponents.visualizeIntComponents() = (this as Components).visualizeComponents()
