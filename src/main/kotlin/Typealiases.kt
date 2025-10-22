d96a742101f1bac3ce6704a9f6d90eb0a4b47f02
Norskeaksel <ahsorbye@gmail.com>
2025-10-22 16:54:39 +0200

    Add ,

--- src/main/kotlin/Typealiases.kt
+++ src/main/kotlin/Typealiases.kt
@@ -17,5 +17,5 @@ fun AdjacencyList.toUnweightedAdjacencyList() = map { edges -> edges.map { it.se
 /** Replaces the destination nodes with edges of weight 1.0 */
 fun UnweightedAdjacencyList.toWeightedAdjacencyList() =
     map { edges -> edges.map { 1.0 to it }.toMutableList() }.toMutableList()
-/** Returns a new, independent identical AdjacencyList */
+/** Returns a new, independent, identical AdjacencyList */
 fun AdjacencyList.deepCopy() = map { it.toMutableList() }.toMutableList()
